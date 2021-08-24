37b81f37457b168af663708be07099c72cff1aaa
AntoineCharette <antoineocharette@gmail.com>
2021-08-24 17:52:49 -0400

    adds working endpoints *, /, /weather

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const weather = require('./data/weather.json');
+
 const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/weather', (request, response) => {
+  response.send({ weather });
+});
+
+app.get('/', (request, response) => {
+  response.send('Hello, World!');
+});
+
+app.get('*', (request, response) => {
+  response.status(404).send('not found');
+});
+
+app.listen(PORT, () => console.log(`Listening to PORT: ${PORT}`));
