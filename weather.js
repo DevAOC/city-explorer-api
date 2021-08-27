50e703fc598b6514fc92908820b3e21124833473
AntoineCharette <antoineocharette@gmail.com>
2021-08-27 18:37:11 -0400

    refactors weather.js

--- weather.js
+++ weather.js
@@ -2,21 +2,13 @@ const axios = require('axios');
 
 const weather = async (request, response) => {
   try {
-    console.log('QUERY', request.query);
     const weatherResp = await axios.get(
       `http://api.weatherbit.io/v2.0/forecast/daily/?lat=${request.query.lat}&lon=${request.query.lon}&key=${process.env.WEATHER_API_KEY}&days=5&lang=en`
     );
-    const cityData = weatherResp.data;
-    if (cityData) {
-      const forecasts = cityData.data.map((day) => new Forecast(day));
-      response.status(200).send(forecasts);
-    } else if (cityData.lat !== request.query.lat || cityData.lon !== request.query.lon) {
-      response.status(404).send('No weather for this location');
-    } else {
-      response.status(500).send('internal server error');
-    }
+    const forecasts = weatherResp.data.data.map((day) => new Forecast(day));
+    response.status(200).send(forecasts);
   } catch (err) {
-    console.log(err);
+    response.send(err);
   }
 };
 
